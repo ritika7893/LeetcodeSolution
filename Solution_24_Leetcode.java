6e96f3b639391b0dea165c0360d6791879303e5d
ritika7893 <158253785+ritika7893@users.noreply.github.com>
2025-03-16 21:28:10 +0530

    Create Solution_24_Leetcode.java

---
+++ Solution_24_Leetcode.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+       if(head==null||head.next==null){
+        return head;
+       } 
+       ListNode dummy=new ListNode(0);
+       ListNode prev=dummy;
+       ListNode curr=head;
+       while(curr!=null && curr.next!=null){
+         ListNode first=curr;
+          ListNode second=curr.next;
+          first.next=second.next;
+          second.next=first;
+          prev.next=second;
+          prev=first;
+          curr=first.next;
+       }
+       return dummy.next;
+    }
+}
